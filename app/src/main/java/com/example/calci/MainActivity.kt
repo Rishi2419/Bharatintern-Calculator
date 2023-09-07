74710a40d6f52610707d38128a0676732cfe8900
Rishi2419 <sahurishikesh1908@gmail.com>
2023-09-07 19:03:20 +0530

    frontend completed

--- app/src/main/java/com/example/calci/MainActivity.kt
+++ app/src/main/java/com/example/calci/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.calci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun clear(view: View) {}
+    fun back(view: View) {}
+    fun operator(view: View) {}
+    fun allclear(view: View) {}
+    fun equal(view: View) {}
+    fun digit(view: View) {}
 }
